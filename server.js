f1f010324294f615c16e9d9eff6d957f327e2268
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-04-08 14:42:17 +0200

    Added MongoDB infos

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var restify = require('restify');
 var app = restify.createServer();
+const { uri, dbName, client } = require('./mongodb-connection')
 
 const PORT = 8080 || process.env.PORT
 
 app.get('/', (req,res) => {
   res.send("Hello world")
+  console.log(uri)
 })
 
 app.listen(PORT, function() {
