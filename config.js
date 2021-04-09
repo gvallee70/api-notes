9e2e38eac700735a136b3f1191d0c6c943be4691
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-04-09 15:24:03 +0200

    Replaced config.js by an .env file

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 8080,
-    db: {
-        name: "notes-api",
-        uri: "mongodb+srv://admin:admin@notes-api.ezzrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    },
-};
